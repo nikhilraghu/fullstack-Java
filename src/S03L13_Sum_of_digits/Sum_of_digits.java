e0c08e61e625d83dd1ee9f1722d699e6350473c4
nikhilraghuwanshi <nikhilraghuwanshi75@gmail.com>
2024-08-29 12:03:21 +0530

    add file

--- src/S03L13_Sum_of_digits/Sum_of_digits.java
+++ src/S03L13_Sum_of_digits/Sum_of_digits.java
@@ -1,7 +1,32 @@
 package S03L13_Sum_of_digits;
+import java.util.Scanner;
 
 public class Sum_of_digits {
     public static void main(String[] args) {
-        System.out.println();
+
+//        int x = 1234; // 1+ 2 + 3 + 4
+//        int sumOfDigits = 0;
+//
+//      while(true) {
+//          sumOfDigits = sumOfDigits + ( x % 10);
+//          x = x/10;
+//          if( x < 1){
+//              break;
+//          }
+//      }
+//        System.out.println("sum is : " + sumOfDigits);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter num: ");
+        int num = sc.nextInt();
+        int sum=0;
+        num = Math.abs(num);
+        while (num!=0){
+            int  Numsum = num%10;
+            sum = sum+Numsum;
+            num = num/10;
+        }
+        System.out.println(sum);
     }
+
 }
